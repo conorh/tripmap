e4df3ed4dc7c9f25bc9db10acffd29e4a221cd22
Conor Hunt <conor.hunt+git@gmail.com>
2009-09-23 00:24:40 -0400

    added gmaps keys as heroku vars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def dist(distance)
     "%.0f" % distance
   end
+  
+  def gmap_key
+    case request.host
+    when 'plotmytrip.heroku.com' then ENV['GMAPS_KEY_HEROKU']
+    when 'plotmytrip.com' then ENV['GMAPS_KEY_ROOT']
+    when 'www.plotmytrip.com' then ENV['GMAPS_KEY_WWW']
+    else
+      'ABQIAAAAzMUFFnT9uH0xq39J0Y4kbhTJQa0g3IQ9GZqIMmInSLzwtGDKaBR6j135zrztfTGVOm2QlWnkaidDIQ'
+    end
+  end
 end
